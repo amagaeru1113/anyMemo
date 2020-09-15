54295a50f00374b6f37d65175ae703e7d6678d6b
steelball <apple@133-175-24-116.tokyo.ap.gmo-isp.jp>
2020-09-15 17:48:34 +0900

    sakai-lesson secton13

---
+++ pythonLesson/section13/socket_server.py
@@ -0,0 +1,31 @@
+"""
+wellknown port (0-1023)
+登録済みport (1024-49151)
+動的・プライベートport (49152-65535)
+
+
+
+"""
+
+import socket
+
+# TCP/IP
+# with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+#     s.bind(("127.0.0.1", 50007))
+#     s.listen(1)
+#     while True:
+#         conn, addr = s.accept()
+#         with conn:
+#             while True:
+#                 data = conn.recv(1024)
+#                 if not data:
+#                     break
+#                 print("data: {}, addr: {}".format(data, addr))
+#                 conn.sendall(b"Received: " + data)
+
+# UDP 受け取ったことを確かめない
+with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as s:
+    s.bind(("127.0.0.1", 50007))
+    while True:
+        data, addr = s.recvfrom(1024)
+        print("data: {}, addr: {}".format(data, addr))
